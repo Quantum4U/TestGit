ccabceefa9bf2613d89f04b4d1a2c6cc3a025d49
rajeev <rajiv.jamia@gmail.com>
2017-05-16 12:15:29 +0530

    Just Testing2

--- app/src/main/java/admobvideo/com/example/rohit/myapplication/testgit/MainActivity.java
+++ app/src/main/java/admobvideo/com/example/rohit/myapplication/testgit/MainActivity.java
@@ -20,13 +20,14 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action>>>>>>>>>>>>>>", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                //line added by rajeev
             }
         });
     }
